35450f2cf753e2acb705b253c10949ba7f5e306f
ViniciusCruzMoura <contato.cruz.moura@gmail.com>
2024-01-18 14:48:57 -0400

    add cache_page

--- apps/management_system/views.py
+++ apps/management_system/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from .models import Animal
+from django.views.decorators.cache import cache_page
 
 # Create your views here.
 
+@cache_page(60)
 def index(request):
     context = {
         'title': 'Sistema de Gestão de Adoção de Animais',
