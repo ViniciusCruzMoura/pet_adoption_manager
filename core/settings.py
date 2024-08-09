50028af3fc6c211c549feab14c1609024d556129
Vinícius Moura <83794355+ViniciusCruzMoura@users.noreply.github.com>
2024-08-09 10:55:12 -0400

    change: log config

--- core/settings.py
+++ core/settings.py
@@ -281,3 +281,17 @@ JAZZMIN_UI_TWEAKS = {
     "theme": "sketchy",
     #"theme": "simplex",
 }
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+}
