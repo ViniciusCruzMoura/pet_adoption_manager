cdef4186783bdd2433caa59c49ab664c1b69c1c6
Vinícius Moura <83794355+ViniciusCruzMoura@users.noreply.github.com>
2024-08-09 10:57:54 -0400

    change: update gunicorn-cfg.py

--- gunicorn-cfg.py
+++ gunicorn-cfg.py
@@ -1,7 +1,8 @@
-#from multiprocessing import cpu_count
-
+from multiprocessing import cpu_count
 bind = '0.0.0.0:8000'
-workers = 1 #2 * cpu_count() + 1
+workers = 2 * cpu_count() + 1
+worker_class = "gthread"
+threads = 2 * cpu_count() + 1
 accesslog = '-'
 loglevel = 'debug'
 capture_output = True
